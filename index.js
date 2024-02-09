e3e157cbb739542f90d13e6b6ab9115c07e21657
nathaliewillemse21 <njpwillemse69@gmail.com>
2024-02-09 16:15:54 +0200

    expressContent

--- index.js
+++ index.js
@@ -1,1 +1,80 @@
-console.log("Welcome to Express JS");
+// console.log("Welcome to Express JS");
+
+import express from 'express'
+
+// const express = require('express') {Common JS}
+
+// Create an express app
+const app = express()
+const port = +process.env.PORT || 4000
+// Router
+
+app.get('/', (req, res) => {
+    res.json({
+        status: res.statusCode,
+        msg: 'You\'re home'
+    })
+//     res.status(200).json({
+//         msg: 'You\'re home'
+})
+app.get('/about', (req, res) => {
+    res.json({
+        status: res.statusCode,
+        msg: 'About page'
+    })
+})
+
+app.all('*', (req, res) => {
+    res.json({
+        status: res.statusCode,
+        msg: '404 Page'
+    })
+})
+// app.get('/jobTiltle')
+app.listen(port)
+
+
+
+/*
+HTTP  Methods
+>GET 
+>POST
+>PUT
+>PATCH {is faster than PUT}
+>DELETE
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
